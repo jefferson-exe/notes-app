5655c47acae470d89099d32c50a588c95dcce366
jefferson-exe <20103827@mail.wit.ie>
2022-10-31 21:48:28 +0000

    Added a addNote function and listNote function in the collection

--- src/main/kotlin/models/Note.kt
+++ src/main/kotlin/models/Note.kt
@@ -2,6 +2,7 @@ package models
 
 data class Note(val noteTitle: String,
                 val notePriority: Int,
+                val noteCategory: String,
                 val isNoteArchived: Boolean){
 
 }
